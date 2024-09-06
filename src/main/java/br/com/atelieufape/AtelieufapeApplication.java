c27af9f681a38284123f0a3f8c2a8fd262558bd7
Thiago Silva <thhzmonster@gmail.com>
2024-09-06 08:40:27 -0300

    Initial commit

---
+++ src/main/java/br/com/atelieufape/AtelieufapeApplication.java
@@ -0,0 +1,13 @@
+package br.com.atelieufape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtelieufapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtelieufapeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/atelieufape/AtelieufapeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.atelieufape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtelieufapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
