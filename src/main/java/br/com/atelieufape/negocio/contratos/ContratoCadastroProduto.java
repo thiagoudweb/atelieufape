1b25664cc766d2c311d2c4eafc72944021c4a2c4
TheSerian <151098795+TheSerian@users.noreply.github.com>
2024-09-16 12:05:59 -0300

    feat: add Produto
    add ProdutoController, ProdutoDados, CadastroProduto, ContratoCadastroProduto. ProdutoAbstract e produto na Fachada

---
+++ src/main/java/br/com/atelieufape/negocio/contratos/ContratoCadastroProduto.java
@@ -0,0 +1,24 @@
+package br.com.atelieufape.negocio.contratos;
+
+import java.util.List;
+
+import br.com.atelieufape.negocio.basico.ProdutoEntity;
+import br.com.atelieufape.negocio.cadastro.exception.CadastroProdutoException;
+
+public interface ContratoCadastroProduto {
+	
+	public ProdutoEntity cadastrarProduto(ProdutoEntity produto) throws CadastroProdutoException;
+	
+	public ProdutoEntity atualizarProduto(ProdutoEntity produto) throws CadastroProdutoException;
+	
+	public void removerProduto(ProdutoEntity produto) throws CadastroProdutoException;
+	
+	public void removerProduto(Long id) throws CadastroProdutoException;
+	
+	public List<ProdutoEntity> listarProdutos() throws CadastroProdutoException;
+	
+	public ProdutoEntity buscarProdutoPorNome (String nome) throws CadastroProdutoException;
+	
+	public ProdutoEntity buscarProdutoPorId(Long id) throws CadastroProdutoException;
+	
+}
--- src/main/java/br/com/atelieufape/negocio/fachada/Fachada.java
+++ src/main/java/br/com/atelieufape/negocio/fachada/Fachada.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.atelieufape.negocio.basico.ProdutoEntity;
 import br.com.atelieufape.negocio.basico.UsuarioEntity;
 import br.com.atelieufape.negocio.basico.UsuarioExpositorEntity;
 import br.com.atelieufape.negocio.cadastro.exception.AtualizarUsuarioException;
 import br.com.atelieufape.negocio.cadastro.exception.CadastroExpositorException;
+import br.com.atelieufape.negocio.cadastro.exception.CadastroProdutoException;
 import br.com.atelieufape.negocio.cadastro.exception.CadastroUsuarioException;
 import br.com.atelieufape.negocio.contratos.ContratoCadastroExpositor;
+import br.com.atelieufape.negocio.contratos.ContratoCadastroProduto;
 import br.com.atelieufape.negocio.contratos.ContratoCadastroUsuario;
 
 @Service
@@ -20,6 +24,9 @@ public class Fachada {
 
 	@Autowired
 	private ContratoCadastroExpositor cadastroExpositor;
+	
+	@Autowired
+	private ContratoCadastroProduto cadastroProduto;
 
 	// @Autowired
 	// private ContratoCarrinhoUsuario cadastroCarrinhoUsuario;
@@ -66,6 +73,27 @@ public class Fachada {
 
 	public UsuarioExpositorEntity buscarUsuarioExpositorPorID(Long id) throws CadastroExpositorException {
 		return this.cadastroExpositor.buscarUsuarioExpositorPorID(id);
+	}
+	
+	//Produto
+	public ProdutoEntity cadastrarProduto(ProdutoEntity produto) throws CadastroProdutoException {
+		return this.cadastroProduto.cadastrarProduto(produto);
+	}
+	
+	public ProdutoEntity buscarProdutoPorID(Long ID) throws CadastroProdutoException {
+		return this.cadastroProduto.buscarProdutoPorId(ID);
+	}
+	
+	public List<ProdutoEntity> listarProdutos() throws CadastroProdutoException{
+		return this.cadastroProduto.listarProdutos();
+	}
+	
+	public ProdutoEntity atualizarProduto(ProdutoEntity produto) throws CadastroProdutoException {
+		return cadastroProduto.atualizarProduto(produto);
+	}
+	
+	public void removerProduto(ProdutoEntity produto) throws CadastroProdutoException {
+		this.cadastroProduto.removerProduto(produto);
 	}
 
 }
