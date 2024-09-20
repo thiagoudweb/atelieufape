d688171bdec5f72eb93c6c74f90866b24bd551c2
Thiago Silva <thhzmonster@gmail.com>
2024-09-20 17:26:53 -0300

    carrinho

--- src/main/java/br/com/atelieufape/negocio/cadastro/CadastroCarrinhoUsuario.java
+++ src/main/java/br/com/atelieufape/negocio/cadastro/CadastroCarrinhoUsuario.java
@@ -1,10 +1,45 @@
 package br.com.atelieufape.negocio.cadastro;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.atelieufape.dados.CarrinhoDados;
+import br.com.atelieufape.negocio.basico.CarrinhoEntity;
+import br.com.atelieufape.negocio.contratos.ContratoCadastroCarrinho;
 
 
 @Service
-public class CadastroCarrinhoUsuario {
+public class CadastroCarrinhoUsuario implements ContratoCadastroCarrinho {
+	
+	@Autowired
+	private CarrinhoDados carrinhoDados;
+
+	@Override
+	public CarrinhoEntity salvarCarrinho(CarrinhoEntity carrinho) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deletarCarrinhoPorId(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public CarrinhoEntity atualizarCarrinho(CarrinhoEntity carrinho) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<CarrinhoEntity> listarCarrinhos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 	
 	
 	
