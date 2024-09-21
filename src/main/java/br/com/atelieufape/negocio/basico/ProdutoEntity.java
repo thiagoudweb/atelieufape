608d4e97e106777c12565e140febdb29bd498aa7
Thiago Silva <thhzmonster@gmail.com>
2024-09-21 20:00:04 -0300

    Revert "Merge pull request #22 from tiagojose76/main"
    This reverts commit 68fb151edc2255a20feb638863e6b5375adc04d9, reversing
    changes made to 2afc42b65f74a1d5ac0904bbc23fc42713354bbe.

--- src/main/java/br/com/atelieufape/negocio/basico/ProdutoEntity.java
+++ src/main/java/br/com/atelieufape/negocio/basico/ProdutoEntity.java
@@ -5,8 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -38,14 +36,6 @@ public class ProdutoEntity {
 
 	@Column(nullable = false)
 	private int quantidade;
-	
-	
-	  @ManyToOne
-	    @JoinColumn(name = "expositor_id", nullable = false)
-	    private UsuarioExpositorEntity expositor; 
-	
-	
-	
 //  o produto aq n vai precisar saber onde ele ta senddo colocado no carrinho. relacionamento unidirecional..
 //	@OneToOne
 //	private ProdutosCarrinhoEntity produtoNoCarrinho;
@@ -146,13 +136,7 @@ public class ProdutoEntity {
 	public void setQuantidade(int quantidade) {
 		this.quantidade = quantidade;
 	}
-    public UsuarioExpositorEntity getExpositor() {
-        return expositor;
-    }
 
-    public void setExpositor(UsuarioExpositorEntity expositor) {
-        this.expositor = expositor;
-    }
 	// método tostring
 	@Override
 	public String toString() {
