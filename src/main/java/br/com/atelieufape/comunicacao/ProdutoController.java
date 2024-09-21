029d1d33a46ca668b5d0ebfadd8d646643659038
TheSerian <151098795+TheSerian@users.noreply.github.com>
2024-09-21 16:38:41 -0300

    refactor: add anotações Swagger em ProdutoController

--- src/main/java/br/com/atelieufape/comunicacao/ProdutoController.java
+++ src/main/java/br/com/atelieufape/comunicacao/ProdutoController.java
@@ -13,9 +13,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.atelieufape.negocio.basico.CompraEntity;
 import br.com.atelieufape.negocio.basico.ProdutoEntity;
 import br.com.atelieufape.negocio.cadastro.exception.CadastroProdutoException;
 import br.com.atelieufape.negocio.fachada.Fachada;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @Controller
 @CrossOrigin(origins = "http://localhost:3000/")
@@ -25,6 +32,11 @@ public class ProdutoController {
 	@Autowired
 	private Fachada fachada;
 
+	@Operation(summary = "Cadastra um novo produto", description = "Cadastra um novo produto no sistema.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Produto cadastrado com sucesso."),
+        @ApiResponse(responseCode = "400", description = "Erro ao cadastrar o produto.", content = @Content)
+    })
 	@PostMapping("/cadastrar")
 	public ResponseEntity<String> cadastrarProduto(@RequestBody ProdutoEntity produto){
 		try {
@@ -35,6 +47,12 @@ public class ProdutoController {
 		}	
 	}
 	
+	@Operation(summary = "Busca um produto por ID", description = "Retorna os detalhes de um produto específico pelo seu ID.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Produto encontrado.", 
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CompraEntity.class))),
+        @ApiResponse(responseCode = "400", description = "Produto não encontrado.", content = @Content)
+    })
 	@GetMapping("/buscar/{id}")
 	public ResponseEntity<?> buscarProdutoPorID(@PathVariable Long id){
 		try {
@@ -44,6 +62,12 @@ public class ProdutoController {
 		}
 	}
 	
+	@Operation(summary = "Busca um produtor por nome", description = "Retorna os detalhes de um produto pelo seu nome.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Produto encontrado.", 
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CompraEntity.class))),
+        @ApiResponse(responseCode = "400", description = "Produto não encontrado.", content = @Content)
+    })
     @GetMapping("/buscarPorNome/{nome}")
     public ResponseEntity<?> buscarProdutoPorNome(@PathVariable String nome) {
         try {
@@ -54,11 +78,19 @@ public class ProdutoController {
         }
     }
 	
+	@Operation(summary = "Lista todos os produtos", description = "Retorna uma lista com todos os produtos cadastrados.")
+    @ApiResponse(responseCode = "200", description = "Lista de produtos retornada com sucesso.", 
+        content = @Content(mediaType = "application/json", schema = @Schema(implementation = CompraEntity.class)))
 	@GetMapping("/listar")
 	public ResponseEntity<List<ProdutoEntity>> listarProdutos() throws CadastroProdutoException{
 		return ResponseEntity.ok(fachada.listarProdutos());
 	}
 	
+	@Operation(summary = "Atualiza um produto.", description = "Atualiza um produto específico pelo ID.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Produto atualizado com sucesso."),
+        @ApiResponse(responseCode = "400", description = "Erro ao atualizar o produto.", content = @Content)
+    })
 	@PatchMapping("/atualizar/{id}")
 	public ResponseEntity<String> atualizarProduto(@PathVariable Long id, 
 			@RequestBody ProdutoEntity produto){
@@ -71,6 +103,11 @@ public class ProdutoController {
 		}
 	}
 	
+	@Operation(summary = "Remove um produto.", description = "Remove um produto específico pelo ID.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Produto removido com sucesso."),
+        @ApiResponse(responseCode = "400", description = "Erro ao remover o produto.", content = @Content)
+    })
 	@DeleteMapping("/remover/{id}")
 	public ResponseEntity<String> removerProduto(@PathVariable Long id){
 		try {
