4388af2a280ba4b57cd2acf0aa5a8ec24f834300
juniorgalindo1990 <156921807+juniorgalindo1990@users.noreply.github.com>
2024-09-23 02:07:07 -0300

    Atualização da fachada, ExpositorController e ProdutoController

--- src/main/java/br/com/atelieufape/comunicacao/ExpositorController.java
+++ src/main/java/br/com/atelieufape/comunicacao/ExpositorController.java
@@ -15,10 +15,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.atelieufape.negocio.basico.CompraEntity;
+import br.com.atelieufape.negocio.basico.ProdutoEntity;
 import br.com.atelieufape.negocio.basico.UsuarioExpositorEntity;
 import br.com.atelieufape.negocio.cadastro.exception.CadastroExpositorException;
 import br.com.atelieufape.negocio.cadastro.exception.ExpositorCpfDuplicadoException;
 import br.com.atelieufape.negocio.cadastro.exception.ExpositorEmailDuplicadoException;
+import br.com.atelieufape.negocio.cadastro.exception.CadastroProdutoException;
 import br.com.atelieufape.negocio.fachada.Fachada;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -35,10 +37,10 @@ public class ExpositorController {
     private Fachada fachada;
 
     @Operation(summary = "Cadastra um novo expositor", description = "Cadastra um novo usuário como expositor no sistema.")
-	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200", description = "Expositor cadastrado com sucesso."),
-			@ApiResponse(responseCode = "400", description = "Erro ao cadastrar o expositor.", content = @Content)
-	})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Expositor cadastrado com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Erro ao cadastrar o expositor.", content = @Content)
+    })
     @PostMapping("/cadastrar")
     public ResponseEntity<String> cadastrarExpositor(@RequestBody UsuarioExpositorEntity expositor) {
         try {
@@ -53,11 +55,26 @@ public class ExpositorController {
         }
     }
 
+    @Operation(summary = "Cadastra um novo produto para o expositor", description = "Cadastra um novo produto associado a um expositor.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Produto cadastrado com sucesso."),
+        @ApiResponse(responseCode = "400", description = "Erro ao cadastrar o produto.", content = @Content)
+    })
+    @PostMapping("/cadastrarProduto")
+    public ResponseEntity<String> cadastrarProduto(@RequestBody ProdutoEntity produto) {
+        try {
+            fachada.cadastrarProduto(produto);
+            return ResponseEntity.ok("Produto cadastrado com sucesso!");
+        } catch (CadastroProdutoException e) {
+            return ResponseEntity.badRequest().body("Erro ao cadastrar o produto!");
+        }
+    }
+
     @Operation(summary = "Busca um expositor por ID", description = "Retorna os detalhes de um expositor específico pelo seu ID.")
-	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200", description = "Expositor encontrado.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CompraEntity.class))),
-			@ApiResponse(responseCode = "400", description = "Expositor não encontrado.", content = @Content)
-	})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Expositor encontrado.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CompraEntity.class))),
+            @ApiResponse(responseCode = "400", description = "Expositor não encontrado.", content = @Content)
+    })
     @GetMapping("/buscar/{id}")
     public ResponseEntity<?> buscarUsuarioExpositorPorID(@PathVariable Long id) {
         try {
@@ -68,17 +85,17 @@ public class ExpositorController {
     }
 
     @Operation(summary = "Lista todos os expositores", description = "Retorna uma lista com todos os expositores cadastrados.")
-	@ApiResponse(responseCode = "200", description = "Lista de expositores retornada com sucesso.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CompraEntity.class)))
+    @ApiResponse(responseCode = "200", description = "Lista de expositores retornada com sucesso.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CompraEntity.class)))
     @GetMapping("/listar")
     public ResponseEntity<List<UsuarioExpositorEntity>> listarExpositores() {
         return ResponseEntity.ok(fachada.listarExpositores());
     }
 
     @Operation(summary = "Atualiza um expositor.", description = "Atualiza um expositor específico pelo ID.")
-	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200", description = "Expositor atualizado com sucesso."),
-			@ApiResponse(responseCode = "400", description = "Erro ao atualizar o expositor.", content = @Content)
-	})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Expositor atualizado com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Erro ao atualizar o expositor.", content = @Content)
+    })
     @PatchMapping("/atualizar/{id}")
     public ResponseEntity<String> atualizarExpositor(@PathVariable Long id,
             @RequestBody UsuarioExpositorEntity expositor) {
@@ -92,14 +109,13 @@ public class ExpositorController {
     }
 
     @Operation(summary = "Remove um expositor.", description = "Remove um expositor específico pelo ID.")
-	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200", description = "Expositor removido com sucesso."),
-			@ApiResponse(responseCode = "400", description = "Erro ao remover o expositor.", content = @Content)
-	})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Expositor removido com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Erro ao remover o expositor.", content = @Content)
+    })
     @DeleteMapping("/remover/{id}")
     public ResponseEntity<String> removerExpositor(@PathVariable Long id) {
         try {
-
             UsuarioExpositorEntity expositor = fachada.buscarUsuarioExpositorPorID(id);
             fachada.removerExpositor(expositor);
             return ResponseEntity.ok("Expositor removido com sucesso!");
